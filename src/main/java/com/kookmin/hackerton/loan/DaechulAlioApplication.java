d699ef925c661fa69850ee42560176e75d222586
joohyosung <116233098+joohyosung@users.noreply.github.com>
2026-06-24 23:13:46 +0900

    Squashed commit of the following:
    commit 433e5aa4d39f7297e36c257cf8d562e15676c4cd
    Author: joohyosung <116233098+joohyosung@users.noreply.github.com>
    Date:   Wed Jun 24 23:08:58 2026 +0900
    
        오류 수정02
    
        - 추천 상세 패널에서 계산 기준 금리와 계산 기준 기간이 비어 보이지 않도록 프론트 표시 로직을 보완함
        - 주택보유수 조건에 `무주택, 1주택`처럼 복수 허용 조건이 포함된 경우 더 넓은 허용 조건부터 판단하도록 필터 로직을 수정함
    
    commit 129a2ae133ebaa30ecffc05fa0cdd7dc035a1ce5
    Author: joohyosung <116233098+joohyosung@users.noreply.github.com>
    Date:   Wed Jun 24 22:54:01 2026 +0900
    
        에러 수정
    
        - 중복 메서드 제거
        - 신규 필드 getter/setter 추가
    
    commit ee941ae1f95c66a196c203114a7c9a6bc335b431
    Author: joohyosung <116233098+joohyosung@users.noreply.github.com>
    Date:   Wed Jun 24 17:43:22 2026 +0900
    
        추천 계산식 수정

--- src/main/java/com/kookmin/hackerton/loan/DaechulAlioApplication.java
+++ src/main/java/com/kookmin/hackerton/loan/DaechulAlioApplication.java
@@ -1,12 +1,16 @@
 package com.kookmin.hackerton.loan;
 
 import com.kookmin.hackerton.loan.config.LoanApiProperties;
+import com.kookmin.hackerton.loan.config.LoanPolicyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(LoanApiProperties.class)
+@EnableConfigurationProperties({
+    LoanApiProperties.class,
+    LoanPolicyProperties.class
+})
 public class DaechulAlioApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/kookmin/hackerton/loan/model/LoanProduct.java
+++ src/main/java/com/kookmin/hackerton/loan/model/LoanProduct.java
@@ -20,6 +20,51 @@ public class LoanProduct {
     private Long maxAnnualIncome;
     private Integer maxCreditGrade;
 
+    private String annualIncomeText;
+    private String incomeText;
+    private String incomeCondition;
+    private String incomeConditionYes;
+    private String incomeConditionNone;
+
+    private String ageText;
+    private String age39Below;
+    private String age40Above;
+    private String age60Above;
+
+    private String creditGradeText;
+    private String creditGrade1;
+    private String creditGrade2;
+    private String creditGrade3;
+    private String creditGrade4;
+    private String creditGrade5;
+    private String creditGrade6;
+    private String creditGrade7;
+    private String creditGrade8;
+    private String creditGrade9;
+    private String creditGradeNoGrade;
+    private String creditGrade1To5;
+    private String creditGrade6ToNoGrade;
+
+    private String loanTargetHouse;
+    private String houseArea;
+    private String houseHoldCount;
+
+    private String productCategory;
+    private String institutionCategory;
+    private String applicationMethod;
+    private String contact;
+    private String relatedSite;
+    private String guaranteeInstitution;
+    private String extraNotes;
+    private String preferentialRateCondition;
+    private String overdueInterestRate;
+    private String earlyRepaymentFee;
+    private String loanAdditionalCost;
+
+    private String repaymentMethod;
+    private String repaymentTermYears;  
+    private String totalLoanTermYears;  
+
     public String getId() {
         return id;
     }
@@ -154,5 +199,342 @@ public class LoanProduct {
 
     public void setMaxCreditGrade(Integer maxCreditGrade) {
         this.maxCreditGrade = maxCreditGrade;
+    }
+
+    public String getCreditGradeFlag(int grade) {
+        switch (grade) {
+            case 1:
+                return creditGrade1;
+            case 2:
+                return creditGrade2;
+            case 3:
+                return creditGrade3;
+            case 4:
+                return creditGrade4;
+            case 5:
+                return creditGrade5;
+            case 6:
+                return creditGrade6;
+            case 7:
+                return creditGrade7;
+            case 8:
+                return creditGrade8;
+            case 9:
+                return creditGrade9;
+            default:
+                return null;
+        }
+    }
+
+    public String getAnnualIncomeText() {
+    return annualIncomeText;
+    }
+
+    public void setAnnualIncomeText(String annualIncomeText) {
+        this.annualIncomeText = annualIncomeText;
+    }
+
+    public String getIncomeText() {
+        return incomeText;
+    }
+
+    public void setIncomeText(String incomeText) {
+        this.incomeText = incomeText;
+    }
+
+    public String getIncomeCondition() {
+        return incomeCondition;
+    }
+
+    public void setIncomeCondition(String incomeCondition) {
+        this.incomeCondition = incomeCondition;
+    }
+
+    public String getIncomeConditionYes() {
+        return incomeConditionYes;
+    }
+
+    public void setIncomeConditionYes(String incomeConditionYes) {
+        this.incomeConditionYes = incomeConditionYes;
+    }
+
+    public String getIncomeConditionNone() {
+        return incomeConditionNone;
+    }
+
+    public void setIncomeConditionNone(String incomeConditionNone) {
+        this.incomeConditionNone = incomeConditionNone;
+    }
+
+    public String getAgeText() {
+        return ageText;
+    }
+
+    public void setAgeText(String ageText) {
+        this.ageText = ageText;
+    }
+
+    public String getAge39Below() {
+        return age39Below;
+    }
+
+    public void setAge39Below(String age39Below) {
+        this.age39Below = age39Below;
+    }
+
+    public String getAge40Above() {
+        return age40Above;
+    }
+
+    public void setAge40Above(String age40Above) {
+        this.age40Above = age40Above;
+    }
+
+    public String getAge60Above() {
+        return age60Above;
+    }
+
+    public void setAge60Above(String age60Above) {
+        this.age60Above = age60Above;
+    }
+
+    public String getCreditGradeText() {
+        return creditGradeText;
+    }
+
+    public void setCreditGradeText(String creditGradeText) {
+        this.creditGradeText = creditGradeText;
+    }
+
+    public String getCreditGrade1() {
+        return creditGrade1;
+    }
+
+    public void setCreditGrade1(String creditGrade1) {
+        this.creditGrade1 = creditGrade1;
+    }
+
+    public String getCreditGrade2() {
+        return creditGrade2;
+    }
+
+    public void setCreditGrade2(String creditGrade2) {
+        this.creditGrade2 = creditGrade2;
+    }
+
+    public String getCreditGrade3() {
+        return creditGrade3;
+    }
+
+    public void setCreditGrade3(String creditGrade3) {
+        this.creditGrade3 = creditGrade3;
+    }
+
+    public String getCreditGrade4() {
+        return creditGrade4;
+    }
+
+    public void setCreditGrade4(String creditGrade4) {
+        this.creditGrade4 = creditGrade4;
+    }
+
+    public String getCreditGrade5() {
+        return creditGrade5;
+    }
+
+    public void setCreditGrade5(String creditGrade5) {
+        this.creditGrade5 = creditGrade5;
+    }
+
+    public String getCreditGrade6() {
+        return creditGrade6;
+    }
+
+    public void setCreditGrade6(String creditGrade6) {
+        this.creditGrade6 = creditGrade6;
+    }
+
+    public String getCreditGrade7() {
+        return creditGrade7;
+    }
+
+    public void setCreditGrade7(String creditGrade7) {
+        this.creditGrade7 = creditGrade7;
+    }
+
+    public String getCreditGrade8() {
+        return creditGrade8;
+    }
+
+    public void setCreditGrade8(String creditGrade8) {
+        this.creditGrade8 = creditGrade8;
+    }
+
+    public String getCreditGrade9() {
+        return creditGrade9;
+    }
+
+    public void setCreditGrade9(String creditGrade9) {
+        this.creditGrade9 = creditGrade9;
+    }
+
+    public String getCreditGradeNoGrade() {
+        return creditGradeNoGrade;
+    }
+
+    public void setCreditGradeNoGrade(String creditGradeNoGrade) {
+        this.creditGradeNoGrade = creditGradeNoGrade;
+    }
+
+    public String getCreditGrade1To5() {
+        return creditGrade1To5;
+    }
+
+    public void setCreditGrade1To5(String creditGrade1To5) {
+        this.creditGrade1To5 = creditGrade1To5;
+    }
+
+    public String getCreditGrade6ToNoGrade() {
+        return creditGrade6ToNoGrade;
+    }
+
+    public void setCreditGrade6ToNoGrade(String creditGrade6ToNoGrade) {
+        this.creditGrade6ToNoGrade = creditGrade6ToNoGrade;
+    }
+
+    public String getLoanTargetHouse() {
+        return loanTargetHouse;
+    }
+
+    public void setLoanTargetHouse(String loanTargetHouse) {
+        this.loanTargetHouse = loanTargetHouse;
+    }
+
+    public String getHouseArea() {
+        return houseArea;
+    }
+
+    public void setHouseArea(String houseArea) {
+        this.houseArea = houseArea;
+    }
+
+    public String getHouseHoldCount() {
+        return houseHoldCount;
+    }
+
+    public void setHouseHoldCount(String houseHoldCount) {
+        this.houseHoldCount = houseHoldCount;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getInstitutionCategory() {
+        return institutionCategory;
+    }
+
+    public void setInstitutionCategory(String institutionCategory) {
+        this.institutionCategory = institutionCategory;
+    }
+
+    public String getApplicationMethod() {
+        return applicationMethod;
+    }
+
+    public void setApplicationMethod(String applicationMethod) {
+        this.applicationMethod = applicationMethod;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getRelatedSite() {
+        return relatedSite;
+    }
+
+    public void setRelatedSite(String relatedSite) {
+        this.relatedSite = relatedSite;
+    }
+
+    public String getGuaranteeInstitution() {
+        return guaranteeInstitution;
+    }
+
+    public void setGuaranteeInstitution(String guaranteeInstitution) {
+        this.guaranteeInstitution = guaranteeInstitution;
+    }
+
+    public String getExtraNotes() {
+        return extraNotes;
+    }
+
+    public void setExtraNotes(String extraNotes) {
+        this.extraNotes = extraNotes;
+    }
+
+    public String getPreferentialRateCondition() {
+        return preferentialRateCondition;
+    }
+
+    public void setPreferentialRateCondition(String preferentialRateCondition) {
+        this.preferentialRateCondition = preferentialRateCondition;
+    }
+
+    public String getOverdueInterestRate() {
+        return overdueInterestRate;
+    }
+
+    public void setOverdueInterestRate(String overdueInterestRate) {
+        this.overdueInterestRate = overdueInterestRate;
+    }
+
+    public String getEarlyRepaymentFee() {
+        return earlyRepaymentFee;
+    }
+
+    public void setEarlyRepaymentFee(String earlyRepaymentFee) {
+        this.earlyRepaymentFee = earlyRepaymentFee;
+    }
+
+    public String getLoanAdditionalCost() {
+        return loanAdditionalCost;
+    }
+
+    public void setLoanAdditionalCost(String loanAdditionalCost) {
+        this.loanAdditionalCost = loanAdditionalCost;
+    }
+
+    public String getRepaymentMethod() {
+        return repaymentMethod;
+    }
+
+    public void setRepaymentMethod(String repaymentMethod) {
+        this.repaymentMethod = repaymentMethod;
+    }
+
+    public String getRepaymentTermYears() {
+        return repaymentTermYears;
+    }
+
+    public void setRepaymentTermYears(String repaymentTermYears) {
+        this.repaymentTermYears = repaymentTermYears;
+    }
+
+    public String getTotalLoanTermYears() {
+        return totalLoanTermYears;
+    }
+
+    public void setTotalLoanTermYears(String totalLoanTermYears) {
+        this.totalLoanTermYears = totalLoanTermYears;
     }
 }
