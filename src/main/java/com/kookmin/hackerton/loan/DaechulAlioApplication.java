b076ddbed31ac4e8eb261b0540f3fbc1f1ecd185
joohyosung <116233098+joohyosung@users.noreply.github.com>
2026-06-26 03:36:13 +0900

    리펙토링: API 호출 자동화 수정

--- src/main/java/com/kookmin/hackerton/loan/DaechulAlioApplication.java
+++ src/main/java/com/kookmin/hackerton/loan/DaechulAlioApplication.java
@@ -5,6 +5,7 @@ import com.kookmin.hackerton.loan.config.LoanPolicyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -14,6 +15,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class DaechulAlioApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(DaechulAlioApplication.class, args);
     }
 }
